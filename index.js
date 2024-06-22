26777994d820c3e0f9c49e69e97d9e0d572bef94
mykaeull <mykaeull99@gmail.com>
2024-06-22 11:50:24 -0300

    renaming routes

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use("/consultations", verifyToken, consultationsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
